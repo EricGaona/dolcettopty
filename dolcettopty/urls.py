9e8a535a7c6f24c69decac5bb9682dab41cccc8d
ericgaona <ericgaona0413@gmail.com>
2020-05-15 23:47:19 +0000

    N-6 creating and coding of the App search

--- dolcettopty/urls.py
+++ dolcettopty/urls.py
@@ -22,6 +22,7 @@ from django.views import static
 from .settings import MEDIA_ROOT
 from products import urls as urls_products
 from cart import urls as urls_cart
+from search import urls as urls_search
 
 
 urlpatterns = [
@@ -31,5 +32,6 @@ urlpatterns = [
     url(r'^home/', include(urls_home)),   
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}), 
 ]
