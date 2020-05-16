1235728481db0de25d68753ad0ac0455e132ee8c
ericgaona <ericgaona0413@gmail.com>
2020-05-16 13:00:26 +0000

    N-7 creating and coding of the App checkout and installed stripe

--- dolcettopty/urls.py
+++ dolcettopty/urls.py
@@ -23,6 +23,7 @@ from .settings import MEDIA_ROOT
 from products import urls as urls_products
 from cart import urls as urls_cart
 from search import urls as urls_search
+from checkout import urls as urls_checkout
 
 
 urlpatterns = [
@@ -32,6 +33,7 @@ urlpatterns = [
     url(r'^home/', include(urls_home)),   
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}), 
 ]
